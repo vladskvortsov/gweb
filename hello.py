584d9187f6aefbd5b1d35f211c614e234c885638
vladskvortsov <vladskvortsov96@gmail.com>
2021-11-30 19:34:40 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-hello
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+  return "Привіт"
+
+@app.route("/Слава Україні")
+def answer():
+  return "Героям Слава!!!"
+
+if __name__ == "__ main__"
+app.run() 
