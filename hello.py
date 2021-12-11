a6234352e2d9cb16d716536ca2602a671cdb7338
vladskvortsov <vladskvortsov96@gmail.com>
2021-12-11 20:33:31 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,11 +2,8 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-color = os.environ.get('APP_COLOR')
-
 @app.route("/")
 def main():
-print (color)
   return "Привіт"
 
 @app.route("/Слава Україні")
